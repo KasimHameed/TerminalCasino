6215d60dcf29ab9dc1801f325bbc2f3c83486427
Kasim Hameed <kasim.hameed@jefferson.kyschools.us>
2023-03-06 09:39:37 -0500

    Inital Commit

---
+++ Coin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerminalCasino
+{
+    internal class Coin
+    {
+        int NumFlips, numHeads, numTails;
+        public Coin(int numFlips)
+        {
+            NumFlips = numFlips;
+        }
+
+        public void Flip()
+        {
+            var rng = new Random();
+            for(int i = 0; i < NumFlips; i++)
+            {
+                switch(rng.Next(2))
+                {
+                    case 0:
+                        Console.Write("T");
+                        numTails++;
+                        break;
+                    case 1:
+                        Console.Write("H");
+                        numHeads++;
+                        break;
+                }
+            }
+            Console.WriteLine($"\nYou got {numHeads} heads and {numTails} tails!\n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using TerminalCasino;
+
+bool exitApp = false;
+
+Console.WriteLine("Let's flip a coin!");
+
+while (!exitApp)
+{
+    Console.WriteLine("How many flips?");
+    try
+    {
+        var input = Console.ReadLine();
+        if(String.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("Hey, don't ignore me!\n");
+        }
+        else if(input == "exit")
+        {
+            exitApp = true;
+        }
+        else if(Convert.ToUInt16(input) != 0)
+        {
+            var coin = new Coin(Convert.ToUInt16(input));
+            coin.Flip();
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"{ex.Message}\n");
+    }
+}
+
+/*
+ * TODO:
+ * Welcome message
+ * give a starting balance
+ * make coin toss bet-able
+ */
